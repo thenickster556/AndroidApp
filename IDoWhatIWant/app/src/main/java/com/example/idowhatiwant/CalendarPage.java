e3f6900a0e0ac36a048fbf54e82b33c76d8c6e5f
thenickster556 <51385259+thenickster556@users.noreply.github.com>
2019-07-15 05:30:50 -0400

    Add files via upload

---
+++ IDoWhatIWant/app/src/main/java/com/example/idowhatiwant/CalendarPage.java
@@ -0,0 +1,30 @@
+package com.example.idowhatiwant;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.CalendarView;
+
+public class CalendarPage extends AppCompatActivity {
+    private  static final String TAG = "CalendarActivity";
+    private CalendarView mCalendarView;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.calendar_page);
+        mCalendarView = findViewById(R.id.calendarView);
+        mCalendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView CalendarView, int year, int month, int dayOfMonth) {
+                String date = year + "/" + month + "/"+ dayOfMonth ;
+                Log.d(TAG, "onSelectedDayChange: yyyy/mm/dd:" + date);
+                Intent intent = new Intent(CalendarPage.this,MainActivity.class);
+                intent.putExtra("date",date);
+                startActivity(intent);
+
+            }
+        });
+    }
+}
